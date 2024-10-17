a6c186dfdfb5259c3e7d655cd83c7d7da596c55f
Isaque Souza <isaquepow12@gmail.com>
2024-10-17 17:25:34 -0300

    feat: add basic token structure with name, symbol, decimals, and initial supply

--- src/BlockfulToken.sol
+++ src/BlockfulToken.sol
@@ -2,10 +2,20 @@
 pragma solidity >=0.8.25;
 
 contract BlockfulToken {
-    constructor(uint256 initialSupply) { }
+    string public name = "Blockful Token";
+    string public symbol = "BFT";
+    uint8 public decimals = 18;
+    uint256 public initialSupply;
 
-    function transfer(address to, uint256 amount) public {
-        // Implement transfer logic
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(uint256 _initialSupply) {
+        initialSupply = _initialSupply * 10 ** uint256(decimals);
+        balanceOf[msg.sender] = initialSupply;
     }
 
     function balanceOf(address account) public view returns (uint256) {
