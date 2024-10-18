5b2aab4fcccbd2b7d5f68b2a74cff9372e0d55c5
Isaque Souza <isaquepow12@gmail.com>
2024-10-18 18:23:04 -0300

    chore: remove comments from the code for clarity

--- test/TokenVendor.t.sol
+++ test/TokenVendor.t.sol
@@ -12,7 +12,7 @@ contract TokenVendorTest is Test {
     address public user1;
 
     uint256 public constant INITIAL_SUPPLY = 1_000_000 * 1e18;
-    uint256 public constant TOKEN_PRICE = 1e16; // Price per token in wei (0.01 ETH per token)
+    uint256 public constant TOKEN_PRICE = 1e16;
 
     receive() external payable {}
 
@@ -25,45 +25,36 @@ contract TokenVendorTest is Test {
     }
 
     function testBuyTokens() public {
-        uint256 ethAmount = 1 ether; // User sends 1 ETH
-        uint256 expectedTokens = (ethAmount * 1e18) / TOKEN_PRICE; // Should receive 100 tokens (with decimals)
+        uint256 ethAmount = 1 ether;
+        uint256 expectedTokens = (ethAmount * 1e18) / TOKEN_PRICE;
 
-        // Fund user1 with ETH
         vm.deal(user1, ethAmount);
 
-        // User1 buys tokens
         vm.prank(user1);
         vendor.buyTokens{ value: ethAmount }();
 
-        // Check user1's token balance
         assertEq(token.balanceOf(user1), expectedTokens);
-        // Check vendor's ETH balance
         assertEq(address(vendor).balance, ethAmount);
     }
 
     function testSellTokens() public {
-        uint256 tokenAmount = 100 * 1e18; // User wants to sell 100 tokens
-        uint256 expectedEth = (tokenAmount * TOKEN_PRICE) / 1e18; // Should receive 1 ETH
+        uint256 tokenAmount = 100 * 1e18;
+        uint256 expectedEth = (tokenAmount * TOKEN_PRICE) / 1e18;
 
-        // First, buy tokens
-        vm.deal(user1, expectedEth); // User1 needs ETH to buy tokens
+        vm.deal(user1, expectedEth);
         vm.startPrank(user1);
 
         vendor.buyTokens{ value: expectedEth }();
 
-        // Approve vendor to spend tokens
         token.approve(address(vendor), tokenAmount);
 
         uint256 initialBalance = user1.balance;
 
-        // Sell tokens
         vendor.sellTokens(tokenAmount);
 
         vm.stopPrank();
 
-        // Check user1's token balance
         assertEq(token.balanceOf(user1), 0);
-        // Check user1's ETH balance change
         assertEq(user1.balance - initialBalance, expectedEth);
     }
 
@@ -103,10 +94,8 @@ contract TokenVendorTest is Test {
 
         vendor.buyTokens{ value: expectedEth }();
 
-        // Approve vendor to spend tokens
         token.approve(address(vendor), tokenAmount);
 
-        // Expect the TokensSold event
         vm.expectEmit(true, true, false, true);
         emit TokensSold(user1, tokenAmount, expectedEth);
 
@@ -118,16 +107,13 @@ contract TokenVendorTest is Test {
     function testWithdrawEvent() public {
         uint256 ethAmount = 1 ether;
 
-        // User1 buys tokens to add ETH to vendor
         vm.deal(user1, ethAmount);
         vm.prank(user1);
         vendor.buyTokens{ value: ethAmount }();
 
-        // Expect the EthWithdrawn event
         vm.expectEmit(true, true, false, true);
         emit EthWithdrawn(owner, ethAmount);
 
-        // Owner withdraws ETH from vendor
         vendor.withdraw();
     }
 
@@ -135,14 +121,12 @@ contract TokenVendorTest is Test {
         uint256 ethAmount = 1 ether;
         uint256 expectedTokens = (ethAmount * 1e18) / TOKEN_PRICE;
 
-        // Usuário compra tokens
         vm.deal(user1, ethAmount);
         vm.startPrank(user1);
         vendor.buyTokens{ value: ethAmount }();
 
         assertEq(token.balanceOf(user1), expectedTokens);
 
-        // Usuário vende tokens
         token.approve(address(vendor), expectedTokens);
         vendor.sellTokens(expectedTokens);
 
@@ -155,10 +139,8 @@ contract TokenVendorTest is Test {
         vm.deal(user1, ethAmount);
         vm.startPrank(user1);
 
-        // Comprar tokens usando uma quantidade alta de gás
         vendor.buyTokens{ value: ethAmount, gas: 5000000 }();
     }
-
 
     function testMultipleUsersBuying() public {
         for (uint i = 1; i <= 10; i++) {
@@ -171,24 +153,23 @@ contract TokenVendorTest is Test {
     }
 
     function testMaximumETHBuy() public {
-        uint256 maxETH = type(uint256).max; // O valor máximo de uint256
+        uint256 maxETH = type(uint256).max;
         vm.deal(user1, maxETH);
         vm.startPrank(user1);
 
-        // Não esperamos um revert aqui, mas é bom verificar se a quantidade calculada é razoável
         vendor.buyTokens{ value: maxETH }();
     }
 
     function testOnlyOwnerSetTokenPrice() public {
-        vm.prank(user1); // Impersona um usuário que não é o proprietário
+        vm.prank(user1);
         vm.expectRevert("Only owner can call this function");
-        vendor.setTokenPrice(2e16); // Tenta alterar o preço dos tokens
+        vendor.setTokenPrice(2e16);
     }
 
     function testOnlyOwnerWithdraw() public {
-        vm.prank(user1); // Impersona um usuário que não é o proprietário
+        vm.prank(user1);
         vm.expectRevert("Only owner can call this function");
-        vendor.withdraw(); // Tenta fazer o saque
+        vendor.withdraw();
     }
 
     function testZeroTokenTransfer() public {
@@ -210,7 +191,6 @@ contract TokenVendorTest is Test {
         vendor.withdraw();
     }
 
-    // Define the events used in the tests
     event TokensPurchased(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
     event TokensSold(address seller, uint256 amountOfTokens, uint256 amountOfETH);
     event EthWithdrawn(address owner, uint256 amount);
